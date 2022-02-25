be96c36955540c6da9cfda9f22cb7d0b1be58b47
Nigel <usipapers@gmail.com>
2022-02-25 10:42:40 -0500

    corrected bugs from previoius upload. County dict stored in .txt file and called using ast. Added comments throughout program. Error message still does not appear when incorrect county requested

--- index.py
+++ index.py
@@ -1,27 +1,37 @@
 import pandas as pd
 from pprint import pprint
-from Voter_Data import counties
+#used to load dictionary from countydict.txt file
+import ast
 
+# loads dictionary from countydict.txt file
+with open("/Users/nigelmeyer/Desktop/Python/Voter_Data/countydict.txt", "r") as data:
+    counties = ast.literal_eval(data.read())
 
-
-
-if __name__ == "__main__":
+# loops program
+while True:
+    # prints list of available counties
     for county in counties.keys():
-            print(county)
+        print(county)
 
+    # main function
     def main():
-        df = pd.read_excel(r'/Users/nigelmeyer/Desktop/Python/Voter_Data/voterstats-20220215-080324.xls')
+        # reads voterstats Excel sheet into a pandas DataFrame
+        df = pd.read_excel(r"/Users/nigelmeyer/Desktop/Python/Voter_Data/voterstats-20220215-080324.xls")
         
-
+        # this asks the user to select a county to load
         def askforcounty():
             x = input("Which county would you like to examine?\n").upper()
+            # This looks for the user's response in the list of available counties
             for county in counties.keys():
+                # if the requested county is found, the df will be printed
                 if x == county:
                     try:
+                        # here, the .iloc command inverts the columns and rows
                         print(df.iloc[counties[x]])
                         input("Press RETURN to continue.")
                         continue
                     except:
+                        # error message if the requested county is not found
                         print("County not found.\n")
-        
         askforcounty()
+    main() 
