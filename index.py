81a7712f0c27aa57e98495b7b5d88a3a5ed38900
Nigel <usipapers@gmail.com>
2022-03-08 18:10:56 -0500

    can print chosen counties but shows all columns for each as bars, need to make those selectable. Graph shows index number, not name

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pprint import pprint
 import time
 from os import sys
+import matplotlib.pyplot as plt
 
 
 # imports countydict.txt file
@@ -33,38 +34,64 @@ def main(df):
 
         printcountychoices(df)
 
-# asks user to list all the counties they wish to compare and waits for "C" to be input to continue
-# this function is called in the printcountychoices() function
-def continuouscounty():
-    county_list = ''
-    while True:
-        county = input(
-                    "List the counties you would like to see and press enter:\n\
-                    Then enter 'C' when you're ready to continue.\n\
-                    Enter 'Q' to quit at anytime\n").upper()
-        county_list += county + ' '
-        if county == 'Q':
-            return county_list
-        elif county == 'C':
-            return county_list      
+
+
+
+    
     
 
 
 # prints the counties the user has requested
 def printcountychoices(df):
-    counties = continuouscounty()
-    if 'Q' in counties:
+    chosen_counties = continuouscounty()
+    if 'Q' in chosen_counties:
             sys.exit(0)
     else:        
         try:
-            print(df[df['County'].isin(counties.split()[:-1])])
+            print(df[df['County'].isin(chosen_counties.split()[:-1])])
+            print_bar_graph(df, chosen_counties)
             input("Press RETURN to continue.")
         except:
             print("Counties not found.\n")
             time.sleep(2)
             return
 
+# asks user to list all the counties they wish to compare and waits for "C" to be input to continue
+# this function is called in the printcountychoices() function
+def continuouscounty():
+    county_list = ''
+    while True:
+        choice = input(
+                    "List the counties you would like to see and press enter:\n\
+                    Then enter 'C' when you're ready to continue.\n\
+                    Enter 'Q' to quit at anytime\n").upper()
+        county_list += choice + ' '
+        if choice == 'Q':
+            return county_list
+        elif choice == 'C':
+            return county_list  
 
+def print_bar_graph(df, chosen_counties):
+    
+    # x = chosen_counties.split()[:-1]
+    # y = df.columns[2:4]
+
+    chosen_plot = df[df['County'].isin(chosen_counties.split()[:-1])]
+    ax = chosen_plot.plot(kind='bar')
+    # ax = df[['Rep','Dem']].plot(x='County', kind='bar', title ="V comp", figsize=(15, 10), legend=True, fontsize=12)
+    ax.set_xlabel("County", fontsize=12)
+    ax.set_ylabel("Registered", fontsize=12)
+    plt.show()
+
+    # print(type(x))
+    # print(x)
+    # print(type(y))
+    # print(y)
+    # plt.bar(x,y)
+    # plt.xlabel("Counties")
+    # plt.ylabel("Number of Registered Voters")
+    # plt.title("Number of Democrats and Republicans in each county")
+    
 
 
 if __name__ == "__main__":
